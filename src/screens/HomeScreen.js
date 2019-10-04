e82355314ac90b9fdee6e70d19cf979df6496ced
Faith Odonghanro <toriboi.fo@gmail.com>
2019-10-05 00:34:55 +0100

    Update: Make background image occupy device height

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -22,42 +22,38 @@ const homeBackgroundImage = require('../../assets/images/pq_dark.jpeg');
 
 export default class HomeScreen extends Component {
   render() {
-    const username = this.props.navigation.getParam('username', '');
     const { navigate } = this.props.navigation;
 
     return (
       <Container>
-        <MyHeader iconName='menu' title='Home' username={username} {...this.props} />
-
-        <Content>
-          <ImageBackground
-            source={homeBackgroundImage}
-            style={importedStyles.backgroundImage}
-            resizeMode='cover'
-          >
-          <View style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.7)', padding: 20, justifyContent: 'space-evenly' }}>
-            <View style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-            }}>
-              <Icon name='person' style={{fontSize: 60, color: colors.gray, alignSelf: 'center'}}/>
-              <Icon name='plus' type='FontAwesome' style={{fontSize: 30, color: colors.gray, alignSelf: 'center'}}/>
-              <Icon name='book' type='FontAwesome' style={{fontSize: 60, color: colors.gray, alignSelf: 'center'}}/>
-              <Icon name='pause' style={{ transform: [{rotate: '90deg'}], fontSize: 30, color: colors.gray, alignSelf: 'center' }}/>
-              <Icon name='lightbulb-on' type='MaterialCommunityIcons' style={{fontSize: 60, color: 'yellow', alignSelf: 'center'}}/>
-            </View>
-
-            <Text style={styles.text}>
-              Learn about different fields and aspects of Computer Science by answering questions in form of quizzes.
-            </Text>
-
-            <Button backgroundColor={colors.themeColor} large onPress={() => navigate('QuizList')}>
-              <Text style={{ flex: 1, color: colors.gray, fontSize: 20, textAlign: 'center' }}>Take A Quiz</Text>
-            </Button>
+        <MyHeader iconName='menu' title='Home' {...this.props} />
+        <ImageBackground
+          source={homeBackgroundImage}
+          style={importedStyles.backgroundImage}
+          resizeMode='cover'
+        >
+        <View style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.7)', padding: 20, justifyContent: 'space-evenly' }}>
+          <View style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
+            <Icon name='person' style={{fontSize: 60, color: colors.gray, alignSelf: 'center'}}/>
+            <Icon name='plus' type='FontAwesome' style={{fontSize: 30, color: colors.gray, alignSelf: 'center'}}/>
+            <Icon name='book' type='FontAwesome' style={{fontSize: 60, color: colors.gray, alignSelf: 'center'}}/>
+            <Icon name='pause' style={{ transform: [{rotate: '90deg'}], fontSize: 30, color: colors.gray, alignSelf: 'center' }}/>
+            <Icon name='lightbulb-on' type='MaterialCommunityIcons' style={{fontSize: 60, color: 'yellow', alignSelf: 'center'}}/>
           </View>
-          </ImageBackground>
-        </Content>
+
+          <Text style={styles.text}>
+            Learn about different fields and aspects of Computer Science by answering questions in form of quizzes.
+          </Text>
+
+          <Button backgroundColor={colors.themeColor} large onPress={() => navigate('Activity', {screen: 'QuizList'})}>
+            <Text style={{ flex: 1, color: colors.gray, fontSize: 20, textAlign: 'center' }}>Take A Quiz</Text>
+          </Button>
+        </View>
+        </ImageBackground>
       </Container>
     );
   }
