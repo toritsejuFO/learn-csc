b935375de539dbb3ef846975dbb707d1f0f41b03
Faith Odonghanro <toriboi.fo@gmail.com>
2019-11-09 02:56:40 +0100

    Implement splash screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,7 +4,6 @@ import {
   StyleSheet,
   Dimensions,
   View,
-  Alert
 } from 'react-native';
 import {
   Container,
@@ -19,9 +18,18 @@ import colors from '../colors';
 import Quiz from '../../assets/js/Quiz';
 
 const homeBackgroundImage = require('../../assets/images/pq_text_light.jpeg');
+const SplashScreenImage = require('../../assets/images/pq_text_dark.jpeg');
 
 export default class HomeScreen extends Component {
+  state = {
+    isSplashVisible: true,
+  }
+
+  hideSplashScreen = () => this.setState({isSplashVisible: false})
+
   componentDidMount = async () => {
+    setTimeout(() => this.hideSplashScreen(), 5000)
+
     // Lock questions once app is launched for the first time
     const questionLock = await AsyncStorage.getItem('@learncsc:questionLock') || null;
     // const questionLock = null;
@@ -46,32 +54,46 @@ export default class HomeScreen extends Component {
 
   render() {
     const { navigate } = this.props.navigation;
+    const {isSplashVisible} = this.state;
+    const SplashScreen = () => (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'black'}}>
+        <Image
+          source={SplashScreenImage}
+          style={{width: deviceWidth, height: 100}}/>
+        <Text style={{color: colors.themeColorLight}}>Learn Computer Science</Text>
+      </View>
+    )
 
     return (
       <Container>
-        <MyHeader iconName='menu' title='Home' {...this.props} />
-        <Image
-          source={homeBackgroundImage}
-          style={{width: deviceWidth, height: 150}}/>
-        <View style={{backgroundColor: colors.themeColorWhite, justifyContent: 'space-evenly', flex: 1 }}>
-          <View style={styles.textContainer}>
-            <Text style={styles.text}>
-              Learn about different fields and aspects of Computer Science by answering questions in form of quizzes.
-            </Text>
-          </View>
+        {isSplashVisible
+          ? <SplashScreen/>
+          : <>
+            <MyHeader iconName='menu' title='Home' {...this.props} />
+            <Image
+              source={homeBackgroundImage}
+              style={{width: deviceWidth, height: 150}}/>
+            <View style={{backgroundColor: colors.themeColorWhite, justifyContent: 'space-evenly', flex: 1 }}>
+              <View style={styles.textContainer}>
+                <Text style={styles.text}>
+                  Learn about different fields and aspects of Computer Science by answering questions in form of quizzes.
+                </Text>
+              </View>
 
-          <View style={styles.iconContainer}>
-            <Icon name='person' style={{fontSize: 30, color: colors.themeColorDark, alignSelf: 'center'}}/>
-            <Icon name='plus' type='FontAwesome' style={{fontSize: 15, color: colors.themeColorDark, alignSelf: 'center'}}/>
-            <Icon name='book' type='FontAwesome' style={{fontSize: 30, color: colors.themeColorDark, alignSelf: 'center'}}/>
-            <Icon name='pause' style={{ transform: [{rotate: '90deg'}], fontSize: 15, color: colors.themeColorDark, alignSelf: 'center' }}/>
-            <Icon name='lightbulb-on' type='MaterialCommunityIcons' style={{fontSize: 30, color: colors.themeColorDark, alignSelf: 'center'}}/>
-          </View>
+              <View style={styles.iconContainer}>
+                <Icon name='person' style={{fontSize: 45, color: colors.themeColorDark, alignSelf: 'center'}}/>
+                <Icon name='plus' type='FontAwesome' style={{fontSize: 15, color: colors.themeColorDark, alignSelf: 'center'}}/>
+                <Icon name='book' type='FontAwesome' style={{fontSize: 45, color: colors.themeColorDark, alignSelf: 'center'}}/>
+                <Icon name='pause' style={{ transform: [{rotate: '90deg'}], fontSize: 15, color: colors.themeColorDark, alignSelf: 'center' }}/>
+                <Icon name='lightbulb-on' type='MaterialCommunityIcons' style={{fontSize: 45, color: colors.themeColorDark, alignSelf: 'center'}}/>
+              </View>
 
-          <Button style={styles.button} backgroundColor={colors.themeColor} large onPress={() => navigate('Activity', {screen: 'QuizList'})}>
-            <Text style={{ flex: 1, color: colors.gray, fontSize: 20, textAlign: 'center' }}>Take A Quiz</Text>
-          </Button>
-        </View>
+              <Button style={styles.button} backgroundColor={colors.themeColor} large onPress={() => navigate('Activity', {screen: 'QuizList'})}>
+                <Text style={{ flex: 1, color: colors.gray, fontSize: 20, textAlign: 'center' }}>Take A Quiz</Text>
+              </Button>
+            </View>
+          </>
+        }
       </Container>
     );
   }
@@ -101,7 +123,10 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    width: deviceWidth/2,
+    width: deviceWidth,
+    padding: 20,
+    // marginLeft: 20,
+    // marginRight: 20,
     alignSelf: 'center',
   },
   button: {
