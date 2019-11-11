9543e623c31243629a4639a7efb841895c3d4b29
Faith Odonghanro <toriboi.fo@gmail.com>
2019-11-11 12:38:07 +0100

    Fix gesture issues with drawer navigation

--- Navigation.js
+++ Navigation.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 import { createDrawerNavigator } from 'react-navigation-drawer';
